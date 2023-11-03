6f954c8f2fbe054ba34bd4be5a72dac799525be5
Yurii Chernilov <zavsek.o2@gmail.com>
2023-11-03 21:49:18 +0200

    Added result

--- src/com/gmail/zavsek_o2/Converter.java
+++ src/com/gmail/zavsek_o2/Converter.java
@@ -9,16 +9,24 @@ public class Converter  {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Оберіть операцію (1 - милі в кілометри, 2 - кілометри в милі): ");
+        System.out.println("Выберите вариант (1 - мили в километры, 2 - километры в мили): ");
         int choice = scanner.nextInt();
 
         if (choice == 1) {
-            System.out.println("Введите расстояние в милях: ");
+            System.out.print("Введите расстояние в милях: ");
             double miles = scanner.nextDouble();
             double kilometers = milesToKilometers(miles);
             System.out.println(miles + " миль = " + kilometers + " километров");
-
+        } else if (choice == 2) {
+            System.out.print("Введите расстояние в километрах: ");
+            double kilometers = scanner.nextDouble();
+            double miles = kilometersToMiles(kilometers);
+            System.out.println(kilometers + " километров = " + miles + " миль");
+        } else {
+            System.out.println("Неправильный вибор");
         }
+
+        scanner.close();
     }
 }
 
