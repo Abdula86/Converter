70156087e91ddb0b8d61564a2cb019d71b471d02
Yurii Chernilov <zavsek.o2@gmail.com>
2023-11-03 21:25:12 +0200

    Add scanner

--- src/Converter.java
+++ src/Converter.java
@@ -1,11 +1,16 @@
 package com.gmail.zavsek_o2;
 
+import java.util.Scanner;
+
 public class Converter  {
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите вариант (1 - Мили в километры, 2 - Километры в мили): ");
+        int choice = scanner.nextInt();
 
     }
 }
